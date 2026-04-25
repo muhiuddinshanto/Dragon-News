25e5293f0733116ceb12ae0c6d849d94a8802b08
muhiuddinshanto <muhiuddinshanto@gmail.com>
2026-04-25 22:59:00 +0600

    Category Added

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,42 @@
 import Image from "next/image";
 
-export default function Home() {
+
+async function getCategory() {
+  const res = await fetch('https://openapi.programming-hero.com/api/news/categories#');
+  const data = await res.json();
+  return data.data;
+
+}
+
+export default async function Home() {
+  const categories = await getCategory();
+  console.log(categories.news_category);
+
   return (
-    <>
-      <h3>Home</h3>
-    </>
+
+    <div className="grid grid-cols-12 gap-4 container mx-auto my-20">
+
+      <div className="font-bold text-4xl  col-span-3 text-center space-y-4">
+        <p className=" bg-[#E7E7E7] font-semibold text-xl py-4 px-8 rounded-xl">All Category</p>
+        <ul className="flex flex-col gap-3">
+        {
+          categories.news_category.map((categorie) => {
+
+            return <li className="text-[#9F9F9F] text-xl text-left px-7" key={categorie.category_id}>{categorie.category_name}</li>
+          })
+        }</ul>
+      </div>
+      <div className="font-bold text-4xl bg-purple-200 col-span-6">
+        Main
+      </div>
+      <div className="font-bold text-4xl bg-yellow-200 col-span-3">
+        Social Icon
+      </div>
+
+
+
+
+
+    </div>
   );
 }
