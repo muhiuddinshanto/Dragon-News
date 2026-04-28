f02693c08b528dcf4dbe473808b7ed71431601f6
muhiuddinshanto <muhiuddinshanto@gmail.com>
2026-04-28 22:33:27 +0600

    commit

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 
-const client = new MongoClient(process.env.Mongodb_URI);
+const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("Dragon-News");
 
 export const auth = betterAuth({
@@ -21,11 +21,13 @@ export const auth = betterAuth({
     },
   },
 
+
+
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
 
 
-  //...
+  
 });
